dffa8dfc8987a8faaa064904ce0e23291803b411
okoro <mi.fracri91@gmail.com>
2026-01-14 12:08:30 +0100

    Add Ship class with unit tests

---
+++ tests/ship.test.js
@@ -0,0 +1,58 @@
+import Ship from "../src/ship";
+
+describe("Ship", () => {
+  describe("constructor", () => {
+    test("creates a ship with given length", () => {
+      const ship = new Ship(4);
+      expect(ship.length).toBe(4);
+    });
+
+    test("initializes hits to 0", () => {
+      const ship = new Ship(3);
+      expect(ship.hits).toBe(0);
+    });
+  });
+
+  describe("hit()", () => {
+    test("increases hit count by 1", () => {
+      const ship = new Ship(3);
+      ship.hit();
+      expect(ship.hits).toBe(1);
+    });
+
+    test("can be called multiple times", () => {
+      const ship = new Ship(3);
+      ship.hit();
+      ship.hit();
+      expect(ship.hits).toBe(2);
+    });
+  });
+
+  describe("isSunk()", () => {
+    test("returns false when hits are less than length", () => {
+      const ship = new Ship(3);
+      ship.hit();
+      expect(ship.isSunk()).toBe(false);
+    });
+
+    test("returns true when hits equal length", () => {
+      const ship = new Ship(2);
+      ship.hit();
+      ship.hit();
+      expect(ship.isSunk()).toBe(true);
+    });
+
+    test("returns true when hits exceed length", () => {
+      const ship = new Ship(2);
+      ship.hit();
+      ship.hit();
+      ship.hit(); // Extra hit beyond length
+      expect(ship.isSunk()).toBe(true);
+    });
+
+    test("returns false for new ship", () => {
+      const ship = new Ship(4);
+      expect(ship.isSunk()).toBe(false);
+    });
+  });
+});
